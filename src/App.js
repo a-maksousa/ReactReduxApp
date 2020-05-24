239f7e8b99f05a333c0fcbb68cf8f935309b6893
Ahmad Maksousa <ah_maksousa@hotmail.com>
2020-05-24 22:55:20 +0300

    Init Empty View

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container fluid">
+      <div className="row">
+        <div className="col-md-3">
+          <div className="ui segment"></div>
+        </div>
+        <div className="col-md-9">
+          <div className="ui segment"></div>
+        </div>
+      </div>
     </div>
   );
 }
